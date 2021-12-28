8131b8a69fc6677c37df35352a4b4920016fcf32
RinHizakura <s921975628@gmail.com>
2021-12-29 00:24:00 +0800

    Register virtual board information to kernel
    After the work, the next step is to provide a i2c driver which can match
    the virtual i2c platform device diva

--- README.md
+++ README.md
@@ -5,11 +5,16 @@
 **WIP**
 
 The vi2c-diva project is trying to implement virtual I2C platform device "diva" and its device
-driver. We'll try to understand the purpose of each component in the Linux I2C subsystem:
+driver. We'll try to understand the purpose of each component in the Linux I2C subsystem.
 
 ![](misc/linux-i2c-subsystem.png)
+
+For traditional chinese users who want to learn more about this project, you
+are welcome to check the [link](https://hackmd.io/@RinHizakura/BJDTZnUsF) for more
+introduction of I2C. Welcome for any advice and discussion, thanks!
 
 ## Reference
 * [Day 10：I2C Driver (Part 1) - 使用 Device Tree 來找 Driver](https://ithelp.ithome.com.tw/articles/10244211)
 * [Implementing I2C device drivers](https://www.kernel.org/doc/html/latest/i2c/writing-clients.html)
+* [How to instantiate I2C devices](https://www.kernel.org/doc/html/latest/i2c/instantiating-devices.html)
 * [linux/drivers/i2c/busses/i2c-bcm2835.c](https://github.com/torvalds/linux/blob/master/drivers/i2c/busses/i2c-bcm2835.c)
--- adap/main.c
+++ adap/main.c
@@ -1,13 +1,14 @@
 #define pr_fmt(fmt) "[i2c-diva-adap] " fmt
 
-#include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/mod_devicetable.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 
 struct diva_i2c_dev {
     struct device *dev;
     struct i2c_adapter adapter;
+    struct i2c_client *client;
 };
 
 static int diva_i2c_xfer(struct i2c_adapter *adap,
@@ -22,8 +23,24 @@ static u32 diva_i2c_func(struct i2c_adapter *adap)
     return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
 }
 
-static struct i2c_algorithm diva_i2c_algo = {.master_xfer = diva_i2c_xfer,
-                                             .functionality = diva_i2c_func};
+static struct i2c_algorithm diva_i2c_algo = {
+    .master_xfer = diva_i2c_xfer,
+    .functionality = diva_i2c_func,
+};
+
+static struct i2c_board_info diva_i2c_board_info = {
+    I2C_BOARD_INFO("vi2c,diva-i2c", 0x1),
+};
+
+static int diva_i2c_add_client(struct diva_i2c_dev *i2c_dev)
+{
+    i2c_dev->client =
+        i2c_new_client_device(&i2c_dev->adapter, &diva_i2c_board_info);
+    if (IS_ERR(i2c_dev->client))
+        return -ENODEV;
+
+    return 0;
+}
 
 static int diva_i2c_probe(struct platform_device *pdev)
 {
@@ -46,21 +63,28 @@ static int diva_i2c_probe(struct platform_device *pdev)
     adap->dev.parent = &pdev->dev;
 
     ret = i2c_add_adapter(adap);
+    if (ret < 0)
+        goto fail_ret;
+
+    ret = diva_i2c_add_client(i2c_dev);
+    if (ret < 0)
+        goto del_adap;
+
     pr_info("Connect to diva %s\n", adap->name);
+    return 0;
+
+del_adap:
+    i2c_del_adapter(adap);
+fail_ret:
     return ret;
 }
 
 static int diva_i2c_remove(struct platform_device *pdev)
 {
+    struct diva_i2c_dev *i2c_dev = dev_get_drvdata(&pdev->dev);
+    i2c_del_adapter(&i2c_dev->adapter);
     return 0;
 }
-
-static const struct of_device_id diva_i2c_of_match[] = {
-    {.compatible = "vi2c,diva-i2c"},
-    {.compatible = "vi2c,vivy-i2c"},
-    {},
-};
-MODULE_DEVICE_TABLE(of, diva_i2c_of_match);
 
 struct platform_driver diva_i2c_driver = {
     .probe = diva_i2c_probe,
@@ -68,7 +92,6 @@ struct platform_driver diva_i2c_driver = {
     .driver =
         {
             .name = "i2c-diva",
-            .of_match_table = diva_i2c_of_match,
         },
 };
 module_platform_driver(diva_i2c_driver);
